f494daf092ed7e8f986a9f1d5eae197c6be7bb95
Tung Nguyen <nthtung08@gmail.com>
2021-02-10 22:40:34 +0700

    added menu item navigate

--- src/components/Directory/index.jsx
+++ src/components/Directory/index.jsx
@@ -31,11 +31,11 @@ class Directory extends Component {
         linkUrl: 'shop/women',
       },
       {
-        title: 'mens',
+        title: 'men',
         imageUrl: '/images/men.png',
         size: 'large',
         id: 5,
-        linkUrl: 'shop/mens',
+        linkUrl: 'shop/men',
       },
     ],
   }
@@ -43,8 +43,8 @@ class Directory extends Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, id, imageUrl, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...props }) => (
+          <MenuItem key={id} {...props} />
         ))}
       </div>
     )
--- src/components/MenuItem/index.jsx
+++ src/components/MenuItem/index.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import { useHistory, useRouteMatch } from 'react-router-dom'
 import './MenuItem.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`menu-item ${size || ''}`.trim()}>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const match = useRouteMatch()
+  const history = useHistory()
+
+  return (
     <div
-      className='background-image'
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    ></div>
-    <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
+      className={`menu-item ${size || ''}`.trim()}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        className='background-image'
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default MenuItem
