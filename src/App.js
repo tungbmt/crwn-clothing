92784c5ba0e761136e965e6028eec05a1b28f12c
Tung Nguyen <nthtung08@gmail.com>
2021-02-11 10:37:30 +0700

    added header component and navigate

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import './App.css'
 
+import Header from './components/Header'
 import HomePage from './pages/HomePage'
 import ShopPage from './pages/ShopPage'
+
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/'>
           <HomePage />
