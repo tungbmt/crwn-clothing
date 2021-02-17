ad268c0d9f93a2dcf694d6b88b62d7ee6ee81262
Tung Nguyen <nthtung08@gmail.com>
2021-02-17 19:54:43 +0700

    added CartItem component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -6,8 +6,8 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   if (existingCartItems) {
     return cartItems.map(cartItem =>
       cartItem.id === cartItemToAdd.id
-        ? { ...cartItems, quantity: cartItem.quantity + 1 }
-        : cartItems
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
     )
   }
 
