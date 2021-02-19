6954a2e96e8c04c133bea44b4809919a3d069c89
Tung Nguyen <nthtung08@gmail.com>
2021-02-19 10:17:31 +0700

    added cart and user selector, modified CartDropdown style

--- src/components/CartDropdown/CartDropdown.js
+++ src/components/CartDropdown/CartDropdown.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import { selectCartItems } from '../../redux/cart/cart.selectors'
 import CartItem from '../CartItem/CartItem'
 import CustomButton from '../CustomButton/CustomButton'
 import './CartDropdown.scss'
@@ -15,8 +17,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 )
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
 })
 
 export default connect(mapStateToProps)(CartDropdown)
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,9 @@ import './Header.scss'
 import { connect } from 'react-redux'
 import CartIcon from '../CartIcon/CartIcon'
 import CartDropdown from '../CartDropdown/CartDropdown'
+import { createStructuredSelector } from 'reselect'
+import { selectCurrentUser } from '../../redux/user/user.selectors'
+import { selectCartHidden } from '../../redux/cart/cart.selectors'
 
 const Header = ({ currentUser, hidden }) => (
   <div className='header'>
@@ -34,9 +37,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 )
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden,
 })
 
 export default connect(mapStateToProps)(Header)
