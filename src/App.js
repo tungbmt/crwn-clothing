6e1e3ced2eca72f291e05cb6eceb6f0fe2d30b56
Tung Nguyen Thanh <tung.nguyenthanh@orientsoftware.com>
2021-03-01 17:22:49 +0700

    removed connect from App

--- src/App.js
+++ src/App.js
@@ -5,20 +5,17 @@ import HomePage from './pages/HomePage/HomePage'
 import ShopPage from './pages/ShopPage/ShopPage'
 import SignInPage from './pages/SignInPage/SignInPage'
 import './App.scss'
-import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
-import { currentUserSelector } from './redux/user/user.selectors'
-import CheckoutPage from './pages/CheckoutPage/CheckoutPage'
+import { useDispatch, useSelector } from 'react-redux'
 import { checkUserSession } from './redux/user/user.actions'
+import CheckoutPage from './pages/CheckoutPage/CheckoutPage'
 
-const App = ({ checkUserSession, currentUser }) => {
+const App = () => {
+  const dispatch = useDispatch()
+  const currentUser = useSelector(state => state.user.currentUser)
+
   useEffect(() => {
-    checkUserSession()
-  }, [checkUserSession])
-
-  // componentWillUnmount() {
-  //   this.unsubscribeFromAuth()
-  // }
+    dispatch(checkUserSession())
+  }, [dispatch])
 
   return (
     <div>
@@ -37,12 +34,4 @@ const App = ({ checkUserSession, currentUser }) => {
   )
 }
 
-const mapStateToProps = createStructuredSelector({
-  currentUser: currentUserSelector,
-})
-
-const mapDispatchToProps = dispatch => ({
-  checkUserSession: () => dispatch(checkUserSession()),
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.scss'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-
 import { store, persistor } from './redux/store'
 import reportWebVitals from './reportWebVitals'
 
